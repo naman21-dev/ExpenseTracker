58681a78a0418a169b67ebdb68c52de21b4c68d3
naman21-dev <namankohli2102@gmail.com>
2026-05-24 14:36:56 +0530

    update cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,10 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
